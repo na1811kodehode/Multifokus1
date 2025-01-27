e381f52a52259a199baca25a6aec9e76faceb436
na1811kodehode <nas.amin.911@gmail.com>
2025-01-27 17:53:04 +0100

    added pictures

--- FotballManager/obj/Debug/net8.0/FotballManager.AssemblyInfo.cs
+++ FotballManager/obj/Debug/net8.0/FotballManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FotballManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462d5e2a7b7ec947308f425f7ac3df7554f46659")]
 [assembly: System.Reflection.AssemblyProductAttribute("FotballManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FotballManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
